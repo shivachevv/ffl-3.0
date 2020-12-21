7231a69bc8d511f0153e6ea2378a713636294fde
shivachevv <31926786+shivachevv@users.noreply.github.com>
2020-12-21 16:38:00 +0200

    update in stats page

--- src/components/Stats/Chart.vue
+++ src/components/Stats/Chart.vue
@@ -7,42 +7,44 @@ export default {
   props: {
     dataSet: {
       type: Array,
-      required: true
+      required: true,
     },
     labels: {
       type: Array,
-      required: true
+      required: true,
     },
     title: {
       type: String,
-      required: true
+      required: true,
     },
   },
   data() {
     return {
+      chartWidth: undefined,
       options: {
-        maintainAspectRatio: false,
+        // maintainAspectRatio: false,
         gridLines: {
-          display: false
+          display: false,
         },
         responsive: true,
+        maintainAspectRatio: false,
         legend: {
           position: "top",
           labels: {
-            fontSize: 16
-          }
+            fontSize: 16,
+          },
         },
         hover: {
-          mode: "label"
+          mode: "label",
         },
         scales: {
           xAxes: [
             {
               display: true,
               scaleLabel: {
-                display: true
-              }
-            }
+                display: true,
+              },
+            },
           ],
           yAxes: [
             {
@@ -52,11 +54,11 @@ export default {
                 steps: 4,
                 stepValue: 10,
                 // max: 100
-              }
-            }
-          ]
-        }
-      }
+              },
+            },
+          ],
+        },
+      },
     };
   },
   computed: {
@@ -69,18 +71,30 @@ export default {
               label: this.title,
               backgroundColor: "#157019",
               borderColor: "none",
-              data: this.dataSet
-            }
-          ]
+              data: this.dataSet,
+            },
+          ],
         };
       } else {
         return "";
       }
-    }
+    },
+  },
+  created() {
+    // const width = window.innerWidth;
+    // this.chartWidth = width > 500 ? '400px' : "100%";
+    // console.log(this.chartWidth);
   },
   mounted() {
     this.renderChart(this.chartdata, this.options);
-  }
+
+    // const charts = document.querySelectorAll("canvas");
+    // charts.forEach(() => {
+      // console.log(chart);
+      // chart.style.height = this.chartWidth;
+      // chart.style.width = this.chartWidth;
+    // });
+  },
 };
 </script>
 
--- src/components/Stats/Stats.vue
+++ src/components/Stats/Stats.vue
@@ -1,12 +1,50 @@
 <template>
-  <div class="stats-container" v-if="statsNames">
+  <div class="container" v-if="statsNames">
+    <div class="stats-header">
+      <h1>FFL Statistics Page</h1>
+    </div>
+    <vs-tabs :color="colorx">
+      <vs-tab @click="colorx = '#3d474d'" label="Players Stats">
+        <div class="con-tab-ejemplo stats-container">
+          <div v-for="stat in statsNames[typeSelected]" :key="stat" class="chart">
+            <Chart
+              :labels="makeGraphData(stats.stats[typeSelected][stat]).names"
+              :dataSet="makeGraphData(stats.stats[typeSelected][stat]).values"
+              :title="titleMap[stat]"
+            ></Chart>
+          </div>
+        </div>
+      </vs-tab>
+
+      <!-- <vs-tab @click="colorx = 'danger'" label="Danger">
+        <div class="con-tab-ejemplo">
+          Danger
+        </div>
+      </vs-tab>
+      <vs-tab @click="colorx = 'warning'" label="Warning">
+        <div class="con-tab-ejemplo">
+          Warning
+        </div>
+      </vs-tab>
+      <vs-tab @click="colorx = 'dark'" label="Dark">
+        <div class="con-tab-ejemplo">
+          Dark
+        </div>
+      </vs-tab>
+        <vs-tab @click="colorx = 'rgb(16, 233, 179)'" label="RGB | HEX">
+        <div class="con-tab-ejemplo">
+          RGB | HEX
+        </div>
+      </vs-tab> -->
+    </vs-tabs>
+    <!-- <div class="stats-container" v-if="statsNames">
     <div v-for="stat in statsNames" :key="stat">
       <Chart
         :labels="makeGraphData(stats.stats[typeSelected][stat]).names"
         :dataSet="makeGraphData(stats.stats[typeSelected][stat]).values"
         :title="titleMap[stat]"
       ></Chart>
-    </div>
+    </div> -->
   </div>
 </template>
 
@@ -20,40 +58,68 @@ export default {
   props: {},
   data() {
     return {
+      colorx: "#3d474d",
       typeSelected: "players",
-      titleMap:{
-        "mostCptPts": "Most Captain Points",
-        "mostPtsInRnd": "Most Points in Round",
-        "mostTimesOver15": "Most Times Over 15 pts",
-        "playersBestCaptainEfficiency": "Players Best Captain Efficiency",
-        "playersDCTop10": "DC Top 10",
-        "playersDLTop10": "DL Top 10",
-        "playersDRTop10": "DR Top 10",
-        "playersEnglandTop10": "England Top 10",
-        "playersFranceTop10": "France Top 10",
-        "playersGKTop10": "GK Top 10",
-        "playersGermanyTop10": "Germany Top 10",
-        "playersItalyTop10": "Italy Top 10",
-        "playersMCTop10": "MC Top 10",
-        "playersMLTop10": "ML Top 10",
-        "playersMRTop10": "MR Top 10",
-        "playersNetherlandsTop10": "Netherlands Top 10",
-        "playersPortugalTop10": "Portugal Top 10",
-        "playersSTTop10": "ST Top 10",
-        "playersSpainTop10": "Spain Top 10",
-        "playersTop10": "Players Top 10",
-        "playersTurkeyTop10": "Turkey Top 10",
-        "userBestScore": "User Best Round Score"
-      }
+      titleMap: {
+        mostCptPts: "Most Captain Points",
+        mostPtsInRnd: "Most Points in Round",
+        mostTimesOver15: "Most Times Over 15 pts",
+        playersBestCaptainEfficiency: "Players Best Captain Efficiency",
+        playersDCTop10: "DC Top 10",
+        playersDLTop10: "DL Top 10",
+        playersDRTop10: "DR Top 10",
+        playersEnglandTop10: "England Top 10",
+        playersFranceTop10: "France Top 10",
+        playersGKTop10: "GK Top 10",
+        playersGermanyTop10: "Germany Top 10",
+        playersItalyTop10: "Italy Top 10",
+        playersMCTop10: "MC Top 10",
+        playersMLTop10: "ML Top 10",
+        playersMRTop10: "MR Top 10",
+        playersNetherlandsTop10: "Netherlands Top 10",
+        playersPortugalTop10: "Portugal Top 10",
+        playersSTTop10: "ST Top 10",
+        playersSpainTop10: "Spain Top 10",
+        playersTop10: "Players Top 10",
+        playersTurkeyTop10: "Turkey Top 10",
+        userBestScore: "User Best Round Score",
+      },
+      statsNames: {
+        players: [
+          "mostPtsInRnd",
+          "userBestScore",
+          "mostCptPts",
+          "playersBestCaptainEfficiency",
+          "playersTop10",
+          "playersGKTop10",
+          "playersDLTop10",
+          "playersDCTop10",
+          "playersDRTop10",
+          "playersMLTop10",
+          "playersMCTop10",
+          "playersMRTop10",
+          "playersSTTop10",
+          "playersEnglandTop10",
+          "playersFranceTop10",
+          "playersGermanyTop10",
+          "playersItalyTop10",
+          "playersSpainTop10",
+          "playersNetherlandsTop10",
+          "playersPortugalTop10",
+          "playersTurkeyTop10",
+          "mostTimesOver15",
+        ],
+      },
     };
   },
   computed: {
     ...mapGetters(["stats"]),
-    statsNames() {
-      if (this.stats) {
-        return Object.keys(this.stats.stats[this.typeSelected]);
-      } else return undefined;
-    },
+    // statsNames() {
+    //   if (this.stats) {
+    //     console.log(Object.keys(this.stats.stats[this.typeSelected]))
+    //     return Object.keys(this.stats.stats[this.typeSelected]);
+    //   } else return undefined;
+    // },
     // mostCptPts() {
     //   if (this.stats) {
     //     const data = Object.values(this.stats.stats["mostCptPts"]).map(
@@ -94,12 +160,38 @@ export default {
 <style scoped lang="scss">
 @import "../../common/breakpoints.scss";
 
-.stats-container {
-  display:flex;
-  flex-direction: row;
-  justify-content: flex-start;
-  align-items: center;
-  width:100%;
-  flex-wrap: wrap;
+.container {
+  width: 80%;
+  @media #{$mobile} {
+    width: 100%;
+  }
+
+  .stats-header {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 20px;
+    background-color: #3d474d;
+    margin: 20px 0 20px 0;
+
+    h1 {
+      font-size: 2rem;
+      color: #c6c6c6;
+    }
+  }
+  .stats-container {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: center;
+    width: 100%;
+    flex-wrap: wrap;
+
+    .chart {
+      width: 100%;
+    }
+  }
 }
 </style>
